ac252422b6ca8162f10676b1e43af85148672b6b
pabartoc <pasigermany@gmail.com>
2025-07-25 06:52:55 +0200

    updated: ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,7 +6,13 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/25 05:47:49 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/07/25 05:47:50 by pabartoc         ###   ########.fr       */
+/*   Updated: 2025/07/25 06:52:04 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+void	*memcpy(void *restrict dst, const void *restrict src, size_t n)
+{
+	
+}
