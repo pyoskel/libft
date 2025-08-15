ec5868644dfc1028f6d926bc120553af0bdb7239
pabartoc <pasigermany@gmail.com>
2025-08-15 20:43:29 +0200

    added: ft_strlcpy

---
+++ ft_strlcpy.c
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 18:36:12 by pabartoc          #+#    #+#             */
+/*   Updated: 2025/08/15 20:43:01 by pabartoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcpy(char *dest, const char *src, size_t size)
+{
+	size_t	src_len;
+	size_t	i;
+
+	src_len = 0;
+	while ()
+	{
+		
+	}
+	return (src_len);
+}
