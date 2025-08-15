c23f8ba6db56bb338f39445baa73c10761eae5db
pabartoc <pasigermany@gmail.com>
2025-08-15 20:44:13 +0200

    added: ft_strlcat

---
+++ ft_strlcat.c
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strlcat.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/08/15 20:39:06 by pabartoc          #+#    #+#             */
+/*   Updated: 2025/08/15 20:41:14 by pabartoc         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+size_t	ft_strlcat(char *dest, const char *src, size_t size)
+{
+	size_t	dest_len;
+	size_t	src_len;
+	int		i;
+
+	i = 0;
+	
+	return ();
+}
