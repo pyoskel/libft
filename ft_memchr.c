6cf5eb9f8b11a80dcef75dd2339cd0cbd38b922a
pabartoc <pasigermany@gmail.com>
2025-08-26 06:47:28 +0200

    updated: ft_memchr

--- ft_memchr.c
+++ ft_memchr.c
@@ -5,14 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/23 04:00:42 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/08/23 04:00:42 by pabartoc         ###   ########.fr       */
+/*   Created: 2025/08/26 06:27:07 by pabartoc          #+#    #+#             */
+/*   Updated: 2025/08/26 06:44:08 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memchr(const void *s, int c, size_t n)
+void	*ft_memchr(const void *s, int c, size_t n)
 {
-	
+	unsigned char	*str;
+	unsigned char	chr;
+	size_t			i;
+
+	str = (unsigned char *) s;
+	chr = (unsigned char) c;
+	i = 0;
+	while (i < n)
+	{
+		if (str[i] == chr)
+			return (&str[i]);
+		i++;
+	}
+	return (NULL);
 }
