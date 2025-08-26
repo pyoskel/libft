f3b1302f22db1acd160833bf5a413c12fdcd34d3
pabartoc <pasigermany@gmail.com>
2025-08-26 06:18:36 +0200

    updated: ft_memcmp

--- ft_memcmp.c
+++ ft_memcmp.c
@@ -5,14 +5,27 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/08/23 07:08:56 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/08/23 07:08:56 by pabartoc         ###   ########.fr       */
+/*   Created: 2025/08/26 06:08:49 by pabartoc          #+#    #+#             */
+/*   Updated: 2025/08/26 06:13:56 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-int	ft_memcmp(const char *s1, const char *s2, size_t n)
+int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
+	unsigned char	*str1;
+	unsigned char	*str2;
+	size_t			i;
 
+	str1 = (unsigned char *) s1;
+	str2 = (unsigned char *) s2;
+	i = 0;
+	while (i < n)
+	{
+		if (str1[i] != str2[i])
+			return (str1[i] - str2[i]);
+		i++;
+	}
+	return (0);
 }
