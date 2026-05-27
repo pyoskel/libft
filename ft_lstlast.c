ec987b80d858a43dce30363f273b6d82872a29bd
pabartoc <pabartoc@student.42heilbronn.de>
2026-05-27 08:06:08 +0200

    Implement functions ft_lstsize, ft_lstlast, ft_lstclear, and ft_lstdelone to expand linked list operations. This enables counting elements, finding the last node, and safely freeing individual nodes or entire lists without memory leaks.

--- ft_lstlast.c
+++ ft_lstlast.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 21:01:20 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/10 21:09:21 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/05/17 23:20:45 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,10 @@
 
 t_list	*ft_lstlast(t_list *lst)
 {
-
+	if (!lst)
+        return (NULL);
+	// Solange der Zeiger Wahr / ungleich 0 ist
+    while (lst->next)
+        lst = lst->next;
+    return (lst);
 }
--- ft_lstsize.c
+++ ft_lstsize.c
@@ -6,7 +6,7 @@
 /*   By: pabartoc <pabartoc@student.42heilbronn.    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/10 21:01:10 by pabartoc          #+#    #+#             */
-/*   Updated: 2025/10/10 21:09:32 by pabartoc         ###   ########.fr       */
+/*   Updated: 2026/05/24 03:50:21 by pabartoc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,13 @@
 
 int	ft_lstsize(t_list *lst)
 {
+	int count;
 
+    count = 0;
+    while (lst != NULL)
+    {
+        count++;
+        lst = lst->next;
+    }
+    return (count);
 }
